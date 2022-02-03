94bebea16c531b5bc213807cc5e63aa5f1e41048
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-02-03 18:51:00 +0000

    fix: move to react

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>TinyMCE Quick Start Guide</h1>
+      <form method="post">
+        <textarea id="mytextarea">Hello, World!</textarea>
+      </form>
+    </div>
+  );
+}
+
+export default App;
