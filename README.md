fb54f26e1f3bb6c031563cf284e899f717730e91
Lou Bichard <contact@louisjohnbichard.co.uk>
2022-02-08 20:47:02 +0000

    fix: updated styles

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # meeting-actions
 
+1. Fix the notes->task UX
+  - #Option 1: Highlight text > Hover a 
+2. Fix the task bar UI
+3. Export (to take the list away and/or copy the notes)
+4. Assignees
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,16 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
-class Button extends React.Component {
-  style() {
-    return {
-      padding: 10,
-      width: "18%",
-      backgroundColor: this.props.color,
-      color: '#00AB91'
-    };
-  }
+const Button = ({ children, onClick, className }) => <button className={className} onClick={onClick}>
+  {children}
+</button>
 
-  render() {
-    return <button style={this.style()} onClick={this.props.onClick}>
-      {this.props.children}
-    </button>
-  }
-}
-
-class TaskInput extends React.Component {
-  style() {
-    return {
-      padding: 10,
-      width: "75%"
-    }
-  }
-
-  render() {
-    return <input
-      style={this.style()}
-      placeholder="New Task..."
-      value={this.props.value}
-      onChange={this.props.onTitleChange}
-    />
-  }
-}
+const TaskInput = ( { value, onTitleChange }) => <input
+  placeholder="New Task..."
+  value={value}
+  onChange={onTitleChange}
+/>
 
 class NewTask extends React.Component {
   constructor() {
@@ -60,76 +35,38 @@ class NewTask extends React.Component {
   render() {
     return <div>
       <TaskInput value={this.state.newTitle} onTitleChange={this.onTitleChange} />
-      <Button onClick={this.addTask}>Create New</Button>
+      <Button className="createNew" onClick={this.addTask}>Create New</Button>
     </div>
   }
 }
 
-class Task extends React.Component {
-  constructor() {
-    super();
-    this.removeItem = this.removeItem.bind(this);
-  }
-  containerStyle() {
-    return {
-      paddingLeft: 15,
-      paddingTop: 10,
-      fontSize: 18,
-    }
-  }
+const Task = ({ removeItem, removeTask, children}) => <div class="taskWrapper">
+  <div className="taskName">{children}</div>
+  <Button className="removeTask" onClick={() => removeTask(children)}>x</Button>
+</div>
 
-  textStyle() {
-    return {
-      display: 'inline-block',
-      width: '75%'
-    }
+const TasksList = ({tasks, onRemovetask}) => {
+  let todoList = [];
+  let key = 0;
+  for (let task of tasks) {
+    todoList.push(
+      <Task key={key++} removeTask={onRemovetask}>
+        {task}
+      </Task>)
   }
-
-  removeItem() {
-    this.props.removeTask(this.props.children);
-  }
-
-  render() {
-    return <div style={this.containerStyle()}>
-      <span style={this.textStyle()}>{this.props.children}</span>
-      <Button onClick={this.removeItem}>x</Button>
-    </div>
-  }
-}
-
-class TasksList extends React.Component {
-  render() {
-    let todoList = [];
-    let key = 0;
-    for (let task of this.props.tasks) {
-      todoList.push(
-        <Task key={key++} removeTask={this.props.onRemovetask}>
-          {task}
-        </Task>)
-    }
-    return <div>{todoList}</div>
-  }
+  return <Fragment>{todoList}</Fragment>
 }
 
 const CustomEditor = ({ onNewTask }) => {
-  const initialValue = "sdfsdff"
-  const [value, setValue] = useState(initialValue ?? '');
-  useEffect(() => setValue(initialValue ?? ''), [initialValue]);
-  const myOnchange = (thing) => {
-      // console.log('loggg', thing)
-      return (event, editor) => { 
-        // console.log('editor all content', editor.getContent()) 
-        const value = editor.getDoc().getSelection().focusNode;
-        console.log('selection content', editor.getDoc().getSelection().focusNode) 
-        // debugger
-        // onNewTask(value.data)
-      }
+  const myOnchange = (_, editor) => { 
+    const { data } = editor.getDoc().getSelection().focusNode;
+    console.log('selection content', editor.getDoc().getSelection().focusNode) 
+    onNewTask(data)
   }
   return (
     <Editor
-      onMouseUp={myOnchange("heyy")}
-      initialValue="my tasks \r sdfsdfsdf \n sdfsdf"
-      // onSelectionChange={(stuff) => { console.log('selected', stuff)} }
+      onMouseUp={myOnchange}
+      initialValue="your tasks here"
       init={{
         height: "100vh",
         menubar: false,
@@ -156,7 +93,7 @@ const CustomEditor = ({ onNewTask }) => {
         // 'bold italic backcolor | alignleft aligncenter ' +
         // 'alignright alignjustify | bullist numlist outdent indent | ' +
         // 'removeformat | help',
-        content_style:'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        content_style:'body { font-family:Helvetica,Arial,sans-serif; font-size:25px }'
       }}
     />
   );
@@ -185,13 +122,16 @@ class App extends React.Component {
 
   render() {
     return <div>
-      <div style={{ height: "calc(100vh)", width: "50%", float: "left" }}>
-        <CustomEditor onNewTask={this.addTask} />
-      </div>
-      <div style={{ width: "50%", float: "right" }}>
-        <NewTask onNewTask={this.addTask} />
-        <TasksList tasks={this.state.tasks} onRemovetask={this.removeTask} />
-      </div>
+        <div className="header"> Meeting Notes App ✅ </div>
+        <div style={{ height: "calc(100vh)", width: "50%", float: "left" }}>
+          <CustomEditor onNewTask={this.addTask} />
+        </div>
+        <div style={{ width: "50%", float: "right" }}>
+          <div className="taskHeader">Task List ({this.state.tasks.length || 0}) 👇</div>
+          <NewTask onNewTask={this.addTask} />
+          <TasksList tasks={this.state.tasks} onRemovetask={this.removeTask} />
+        </div>
+      <div/>
     </div>;
   }
 }
